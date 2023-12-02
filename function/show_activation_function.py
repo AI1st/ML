9eedf2b3e0c519d673ddcee06cf8dd7ef6e97b52
bear <2276114839@qq.com>
2023-12-03 04:42:45 +0800

    adjust activation_function_plot / implemented train codes for relpu

--- function/show_activation_function.py
+++ function/show_activation_function.py
@@ -1,26 +1,38 @@
 import torch
-from graph_plot import plot
+from Common.function.graph_plot import plot
+import torch.nn.functional as F
+from d2l import torch as d2l
 
 
 # 可视化激活函数函数
-def plot_activation_function(model, sequence, deep):
-    x_w = torch.linspace(-500, 500, 500)
-    x_n = torch.linspace(-10, 10, 100)
-    y_w = model(x_w).detach().numpy()
-    y_n = model(x_n).detach().numpy()
-    plot(x_w, y_w, xlabel="input", ylabel="output", title=f"deep:{deep}, layer:{sequence}  wide range output")
-    plot(x_n, y_n, xlabel="input", ylabel="output", title=f"deep:{deep}, layer:{sequence}  narrow range output")
+def compute_error_from_linear(x, y):
+    return torch.sqrt(F.mse_loss(x, y))
 
 
-def show_activation(model, activation, deep=0):
+def plot_activation_function(model, sequence, deep, device):
+    x_w = torch.linspace(-500, 500, 500, device=device)
+    x_n = torch.linspace(-10, 10, 100, device=device)
+    y_w = model(x_w).detach().cpu()
+    y_n = model(x_n).detach().cpu()
+    x_w = x_w.cpu()
+    x_n = x_n.cpu()
+    error_w = compute_error_from_linear(x_w,y_w)
+    error_n = compute_error_from_linear(x_n,y_n)
+    params = [str(round(i.detach().float().item(), 3)) for i in model.parameters()]
+    plot(x_w, y_w, xlabel="input", ylabel="output",
+         title=f"deep:{deep}, layer:{sequence}, error:{round(error_w.item(), 3)},\n p:{', '.join(params)}")
+    plot(x_n, y_n, xlabel="input", ylabel="output",
+         title=f"deep:{deep}, layer:{sequence}, error:{round(error_n.item(), 3)},\n p:{', '.join(params)}")
+
+
+def show_activation(model, activation, deep=0, device=d2l.try_gpu()):
     i = 1
     for layer in model:
         if isinstance(layer, activation):
-            print(layer)
-            plot_activation_function(layer, i, deep)
+            plot_activation_function(layer, i, deep, device=device)
             i += 1
         else:
             try:
                 show_activation(layer, activation, deep + 1)
-            except:
+            except Exception as e:
                 pass
